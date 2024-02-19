7835783d6717db32f7d24bcdc5c86ef0b963fc60
Zachary Robinson <me@zsrobinson.com>
2024-02-19 13:46:17 -0500

    revert "add custom title for play page"
    This reverts commit 76e347a52547d3aea524da3d188a6a4e1d8bc9ca.

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useAutoAnimate } from "@formkit/auto-animate/react";
-import { Metadata } from "next";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { ComponentPropsWithoutRef, useEffect, useState } from "react";
@@ -12,10 +11,6 @@ import { GameOptions, gameOptionsSchema } from "~/lib/game-options";
 import { alphabetical } from "~/lib/utils";
 
 /* eslint-disable react-hooks/exhaustive-deps */
-
-export const metadata: Metadata = {
-  title: "Play my game of Custom Connections!",
-};
 
 export default function Page() {
   const params = useSearchParams();
