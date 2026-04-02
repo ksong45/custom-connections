971150a18cca20c7e8b78563d2bc6f532ce5e4a6
Kyle Song <kylesong45@gmail.com>
2026-04-01 20:11:59 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Suspense, type ReactNode } from "react";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 /* eslint-disable @next/next/no-img-element */
 
